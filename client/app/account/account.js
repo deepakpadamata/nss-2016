b3971bc977604cc3deb5b4af6ff3653e571e7a3a
muthukumar97 <rmuthukumar15@gmail.com>
2015-09-13 18:29:10 +0530

    Added preferences

--- client/app/account/account.js
+++ client/app/account/account.js
@@ -8,6 +8,11 @@ angular.module('nss2016App')
         templateUrl: 'app/account/login/login.html',
         controller: 'LoginCtrl'
       })
+      .state('preferences', {
+        url: '/preferences',
+        templateUrl: 'app/account/preferences/preferences.html',
+        controller: 'PreferencesCtrl'
+      })
       .state('signup', {
         url: '/signup',
         templateUrl: 'app/account/signup/signup.html',
